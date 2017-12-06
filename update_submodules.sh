9ddf452d89ba1926cf76eb5c6424a45b81c3ca02
Mauro Leggieri <mxmauro@mauroleggieri.com>
2017-12-06 19:15:42 -0300

    Removed submodules in favor of subtree (step 2)

---
+++ update_submodules.sh
@@ -0,0 +1,3 @@
+git subtree pull --prefix Source/ZipLib/Source         https://github.com/madler/zlib       master --squash
+git subtree pull --prefix Source/OpenSSL/Source        https://github.com/openssl/openssl   master --squash
+git subtree pull --prefix Source/JsLib/DukTape/Source  https://github.com/svaarala/duktape  master --squash
