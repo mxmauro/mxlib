9240a92e43ec9663f187bc08f4e927100acf8e6b
Mauro Leggieri <mxmauro@gmail.com>
2024-02-23 22:29:06 -0300

    Squashed 'Source/ZipLib/MiniZipSource/' changes from 680d6f1dcf..13b01a74a4
    13b01a74a4 Version 4.0.4.
    d1f4d18899 Update comments style in various code files
    0211461993 Remove DOS line endings
    782b139ee7 Silence -Wunused-but-set-variable warnings
    487259b09a Silence MZ_COMPAT_VERSION warnings
    bacf5122ac Fixed compat layer structures layout and types. #749
    6c5f265a55 Added back unzLocateFile variant for minizip 1.2 with MZ_COMPAT_VERSION.
    fbd8443122 Fix unzLocateFile 3rd parameter
    2c2d6e5940 Reject overflows of zip header fields in minizip compat
    ef3ef9a4b8 Version 4.0.3.
    933cedf2bb Added poc zip file for unzip fuzzer from GH issue #739.
    3417608874 Check for zero length path in mz_path_has_slash
    42b6b8997c Added poc zip file for unzip fuzzer from GH issue #740.
    3920698d13 Fix for filenames of the form "x/../y"
    97d8e65dc8 Match default compression level value in compress streams #726
    1e9c6d8361 Use ZSTD compression level defaults. #726
    2a5543a2b0 Fixed possible memory leak if realloc fails.
    4f670cc7d7 Version 4.0.2.
    145754983c Get mz_dir_make to test for an empty path
    26c484d7ce Fix lzma compression level #731
    0d81d2eaae Added ioapi.h shim for backwards compatibility.
    aa4f87309d Silence warnings about BSD_SOURCE being deprecated.
    2bfdd6e6a7 Fixed implicit conversion precision warnings
    7cb316f28b Fix pkgconfig file for private dependencies
    21606891ab Support compression levels >= 10 and use zstd's internal default level.
    
    git-subtree-dir: Source/ZipLib/MiniZipSource
    git-subtree-split: 13b01a74a44108557e00c78ffac54e4fae7f8c7f

--- mz.h
+++ mz.h
@@ -14,8 +14,8 @@
 /***************************************************************************/
 
 /* MZ_VERSION */
-#define MZ_VERSION                      ("4.0.1")
-#define MZ_VERSION_BUILD                (040001)
+#define MZ_VERSION                      ("4.0.4")
+#define MZ_VERSION_BUILD                (040004)
 
 /* MZ_ERROR */
 #define MZ_OK                           (0)  /* zlib */
